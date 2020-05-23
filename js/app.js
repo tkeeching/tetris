8cee793f0b4aaf55df4793799fa591e35d21da3c
Kee Tee <tkeeching@gmail.com>
2020-05-23 16:29:54 +1000

    Add remaining tetrominoes

--- js/app.js
+++ js/app.js
@@ -6,62 +6,94 @@ document.addEventListener('DOMContentLoaded', () => {
   const rowHeight = 10;
 
   const lTetromino = [  
-    [6, rowHeight+4, rowHeight+5, rowHeight+6], // [6, 14, 15, 16]
-    [5, rowHeight+5, rowHeight*2+5, rowHeight*2+6], // [5, 15, 25, 26]
-    [rowHeight+4, rowHeight+5, rowHeight+6, rowHeight*2+4], // [14, 15, 16, 24]
-    [4, 5, rowHeight+5, rowHeight*2+5] // [4, 5, 15, 25]
+    [2, rowHeight, rowHeight+1, rowHeight+2], // [6, 14, 15, 16]
+    [1, rowHeight+1, rowHeight*2+1, rowHeight*2+2], // [5, 15, 25, 26]
+    [rowHeight, rowHeight+1, rowHeight+2, rowHeight*2], // [14, 15, 16, 24]
+    [0, 1, rowHeight+1, rowHeight*2+1] // [4, 5, 15, 25]
   ]
 
   const tTetromino = [  
-    [], // [5, 14, 15, 16]
-    [], // [5, 15, 16, 25]
-    [], // [14, 15, 16, 25]
-    [] // [5, 14, 15, 25]
+    [1, rowHeight, rowHeight+1, rowHeight+2], // [5, 14, 15, 16]
+    [1, rowHeight+1, rowHeight+2, rowHeight*2+1], // [5, 15, 16, 25]
+    [rowHeight, rowHeight+1, rowHeight+2, rowHeight*2+1], // [14, 15, 16, 25]
+    [1, rowHeight, rowHeight+1, rowHeight*2+1] // [5, 14, 15, 25]
   ]
 
   const zTetromino = [  
-    [], // [5, 6, 14, 15]
-    [], // [4, 14, 15, 25]
-    [], // [5, 6, 14, 15]
-    [] // [4, 14, 15, 25]
+    [1, 2, rowHeight, rowHeight+1], // [5, 6, 14, 15]
+    [0, rowHeight, rowHeight+1, rowHeight*2+1], // [4, 14, 15, 25]
+    [1, 2, rowHeight, rowHeight+1], // [5, 6, 14, 15]
+    [0, rowHeight, rowHeight+1, rowHeight*2+1] // [4, 14, 15, 25]
   ]
 
   const oTetromino = [  
-    [], // [5, 6, 15, 16]
-    [], // [5, 6, 15, 16]
-    [], // [5, 6, 15, 16]
-    [], // [5, 6, 15, 16]
-
+    [1, 2, rowHeight+1, rowHeight+2], // [5, 6, 15, 16]
+    [1, 2, rowHeight+1, rowHeight+2], // [5, 6, 15, 16]
+    [1, 2, rowHeight+1, rowHeight+2], // [5, 6, 15, 16]
+    [1, 2, rowHeight+1, rowHeight+2] // [5, 6, 15, 16]
   ]
 
   const iTetromino = [  
-    [], // [3, 4, 5, 6]
-    [], // [4, 14, 24, 34]
-    [], // [3, 4, 5, 6]
-    [], // [4, 14, 24, 34]
+    [0, 1, 2, 3], // [3, 4, 5, 6]
+    [1, rowHeight+1, rowHeight*2+1, rowHeight*3+1], // [4, 14, 24, 34]
+    [0, 1, 2, 3], // [3, 4, 5, 6]
+    [1, rowHeight+1, rowHeight*2+1, rowHeight*3+1] // [4, 14, 24, 34]
   ]
   
   const tetrominoes = [lTetromino, tTetromino, zTetromino, oTetromino, iTetromino];
   
-  let currentPosition = 0, nextPosition;
-  let rotation = Math.floor(Math.random() * lTetromino.length);
-  let current = tetrominoes[0][rotation];
+  let currentPosition = 3;
+  let selectTetromino = Math.floor(Math.random() * tetrominoes.length);
+  let tetrominoRotation = Math.floor(Math.random() * lTetromino.length);
+  let currentTetromino = tetrominoes[selectTetromino][tetrominoRotation];
 
   function draw() {
-    current.forEach(index => {
-      cells[currentPosition + index].classList.add('tetromino');
+    currentTetromino.forEach(index => {
+      switch (selectTetromino) {
+        case 0:
+          cells[currentPosition + index].classList.add('ltetromino');
+          break;
+        case 1:
+          cells[currentPosition + index].classList.add('ttetromino');
+          break;
+        case 2:
+          cells[currentPosition + index].classList.add('ztetromino');
+          break;
+        case 3:
+          cells[currentPosition + index].classList.add('otetromino');
+          break;
+        case 4:
+          cells[currentPosition + index].classList.add('itetromino');
+          break;
+      }
     })
   }
   
   function undraw() {
-    current.forEach(index => {
-      cells[currentPosition + index].classList.remove('tetromino');
+    currentTetromino.forEach(index => {
+      switch (selectTetromino) {
+        case 0:
+          cells[currentPosition + index].classList.remove('ltetromino');
+          break;
+        case 1:
+          cells[currentPosition + index].classList.remove('ttetromino');
+          break;
+        case 2:
+          cells[currentPosition + index].classList.remove('ztetromino');
+          break;
+        case 3:
+          cells[currentPosition + index].classList.remove('otetromino');
+          break;
+        case 4:
+          cells[currentPosition + index].classList.remove('itetromino');
+          break;
+      }
     })
     currentPosition += rowHeight;
   }
 
   function moveDown() {
-    if (currentPosition != 160) {
+    if (currentPosition < 160) {
       undraw();
       draw();
     }

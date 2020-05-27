2e3de12d954b2e97cc31980da33702b7eb2160c6
Kee Tee <tkeeching@gmail.com>
2020-05-27 21:10:02 +1000

    Complete core functionality

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const scoreBoard = document.querySelector('.score-board');
   const startBtn = document.querySelector('.start-btn');
   const rowHeight = 10;
+  let timerId = null;
+  let score = 0;
 
   const lTetromino = [  
     [2, rowHeight, rowHeight+1, rowHeight+2], // [6, 14, 15, 16]
@@ -42,11 +44,11 @@ document.addEventListener('DOMContentLoaded', () => {
   
   const tetrominoes = [lTetromino, tTetromino, zTetromino, oTetromino, iTetromino];
   
-  let currentPosition = 3;
   let selectTetromino = Math.floor(Math.random() * tetrominoes.length);
   let tetrominoRotation = 0;
   let currentTetromino = tetrominoes[selectTetromino][tetrominoRotation];
 
+  let currentPosition = 3;
   function draw() {
     currentTetromino.forEach(index => {
       switch (selectTetromino) {
@@ -98,18 +100,164 @@ document.addEventListener('DOMContentLoaded', () => {
       freeze();
   }
 
+  // assign function to keycodes
+  // arrowLeft key: 37
+  // arrowUp key: 38
+  // arrowRight key: 39
+  // arrowDown key: 40
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+
+  document.addEventListener('keyup', control);
+
+  let selectUpNextTetromino = 0;
   function freeze() {
     if (currentTetromino.some(index => cells[currentPosition + index + rowHeight].classList.contains('taken'))) {
       currentTetromino.forEach(index => cells[currentPosition + index].classList.add('taken'))
       // start a new tetromino falling
-      selectTetromino = Math.floor(Math.random() * tetrominoes.length);
+      selectTetromino = selectUpNextTetromino;
+      selectUpNextTetromino = Math.floor(Math.random() * tetrominoes.length);
       currentTetromino = tetrominoes[selectTetromino][tetrominoRotation];
       currentPosition = 3;
       draw();
+      displayNext();
+      addScore();
+      gameOver();
     }
   }
 
-  draw();
-  let timerId = setInterval(moveDown, 800);
+  // move tetromino left, unless it is at the edge or there is  a blockage
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = currentTetromino.some(index => (currentPosition + index) % rowHeight === 0);
+    if (!isAtLeftEdge) currentPosition -= 1;
+    if (currentTetromino.some(index => cells[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  // move tetromino right, unless it is at the edge or there is  a blockage
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = currentTetromino.some(index => (currentPosition + index) % rowHeight === rowHeight - 1);
+    if (!isAtRightEdge) currentPosition += 1;
+    if (currentTetromino.some(index => cells[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    tetrominoRotation++;
+    if (tetrominoRotation === currentTetromino.length) {  // if rotation gets to 4, make it go back to 0
+      tetrominoRotation = 0;
+    }
+    currentTetromino = tetrominoes[selectTetromino][tetrominoRotation];
+    draw();
+  }
+
+  // show up-next tetromino in up-next display
+  const upNextCells = document.querySelectorAll('.up-next-display div');
+  const upNextCellWidth = 4;
+  let upNextIndex = 0;
+
+  // tetrominoes without rotation
+  const upNextTetrominoes = [
+    [8, 11, 12, 13],
+    [7, 11, 12, 13],
+    [7, 8, 11, 12],
+    [7, 8, 12, 13],
+    [11, 12, 13, 14]
+    // tetrominoes[0][0],
+    // tetrominoes[1][0],
+    // tetrominoes[2][0],
+    // tetrominoes[3][0],
+    // tetrominoes[4][0]
+  ]
+
+  // display tetromino in up next display
+  function displayNext() {
+    // remove any trace of a tetromino from the entire grid
+    upNextCells.forEach(cell => {
+      cell.classList.remove('ltetromino');
+      cell.classList.remove('ttetromino');
+      cell.classList.remove('ztetromino');
+      cell.classList.remove('otetromino');
+      cell.classList.remove('itetromino');
+    })
+
+    upNextTetrominoes[selectUpNextTetromino].forEach(index => {
+      switch (selectUpNextTetromino) {
+        case 0:
+          upNextCells[upNextIndex + index].classList.add('ltetromino');
+          break;
+        case 1:
+          upNextCells[upNextIndex + index].classList.add('ttetromino');
+          break;
+        case 2:
+          upNextCells[upNextIndex + index].classList.add('ztetromino');
+          break;
+        case 3:
+          upNextCells[upNextIndex + index].classList.add('otetromino');
+          break;
+        case 4:
+          upNextCells[upNextIndex + index].classList.add('itetromino');
+          break;
+      }
+    });
+  }
+
+  // add event listener to start button
+  startBtn.addEventListener('click', () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      // selectUpNextTetromino = Math.floor(Math.random() * tetrominoes.length);
+      displayNext();       
+    }
+  })
+
+  // add score
+  function addScore() {
+    for (let i = 0; i < 199; i += rowHeight) {
+      const rows = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
   
+      if (rows.every(index => cells[index].classList.contains('taken'))) {
+        score += 10;
+        scoreBoard.innerHTML = score;
+        rows.forEach(index => {
+          cells[index].classList.remove('taken');
+          cells[index].classList.remove('ltetromino');
+          cells[index].classList.remove('ttetromino');
+          cells[index].classList.remove('ztetromino');
+          cells[index].classList.remove('otetromino');
+          cells[index].classList.remove('itetromino');
+        })
+        const cellsRemoved = cells.splice(i, rowHeight);
+        cells = cellsRemoved.concat(cells);
+        cells.forEach(cell => gameBoard.appendChild(cell))
+      }
+    }
+  }
+
+  function gameOver() {
+    if (currentTetromino.some(index => cells[currentPosition + index].classList.contains('taken'))) {
+      scoreBoard.innerHTML = 'end';
+      clearInterval(timerId);
+    }
+  }
 })
